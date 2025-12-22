a4578c45630ecadfacf5b2117db926d1a1f1b2f6
Chi-Hyun <139492649+chihyunkim@users.noreply.github.com>
2025-12-22 14:49:12 -0500

    Prepare data

--- r/export_to_geojson.R
+++ r/export_to_geojson.R
@@ -14,92 +14,127 @@ setwd(dirname(this.path::this.dir()))
 
 data_dir <- str_c(getwd(), "/data")
 
-# Geographic data -----------------------------------------------------------------------------
+# Arrivals data -----------------------------------------------------------------------------
 
-districts <- read_sf(str_c(data_dir, "raw", "geographic_areas", "Pennsylvania_Senate_Districts.geojson", sep = "/")) %>% 
-  # Only select SEPTA-area districts
-  filter(LEG_DISTRICT_NO %in% c(1:9, 10, 12, 17, 19, 24, 26, 44)) %>% 
+arrivals_pre_raw <- data.table::fread(str_c(data_dir, "raw", "arrivals_data", "stop_observations_week1.csv", sep = "/")) %>% 
   clean_names()
 
-# Pre-cut data --------------------------------------------------------------------------------
+arrivals_post_raw <- data.table::fread(str_c(data_dir, "raw", "arrivals_data", "stop_observations_week2.csv", sep = "/")) %>% 
+  clean_names()
 
-normal_raw <- read_gtfs(str_c(data_dir, "raw", "septa_normal", "google_bus.zip", sep = "/"))
+# Check which route/stop combinations are present in both data
+stops_pre <- arrivals_pre_raw %>% 
+  distinct(route_id, stop_id, stop_name)
+
+stops_post <- arrivals_post_raw %>% 
+  distinct(route_id, stop_id, stop_name)
+
+stops_good <- inner_join(stops_pre, stops_post)
+
+# inner_join: added no columns
+# > rows only in stops_pre  (1,430)
+# > rows only in stops_post (  154)
+# > matched rows             6,809
+# >                         =======
+# > rows total               6,809
+
+# Get arrival times for routes/stops which were served both pre- and post-cuts
+arrivals_filtered_pre <- arrivals_pre_raw %>% 
+  # filter: removed 161,960 rows (7%), 2,069,834 rows remaining
+  filter(route_id %in% stops_good$route_id) %>% 
+  # filter: removed 28,827 rows (1%), 2,041,007 rows remaining
+  filter(stop_id %in% stops_good$stop_id) %>% 
+  mutate(period = "before_cuts")
+
+arrivals_filtered_post <- arrivals_post_raw %>% 
+  # filter: no rows removed
+  filter(route_id %in% stops_good$route_id) %>% 
+  # filter: removed 20,020 rows (1%), 1,830,552 rows remaining
+  filter(stop_id %in% stops_good$stop_id) %>% 
+  mutate(period = "during_cuts")
+
+arrivals_ready <- arrivals_filtered_pre %>% 
+  bind_rows(arrivals_filtered_post) %>% 
+  mutate(day_of_week = wday(service_date, label = TRUE)) %>% 
+  mutate(week_period = if_else(day_of_week %in% c("Sat", "Sun"), "Weekend", "Weekday")) %>% 
+  mutate(arrival_hour = as.numeric(str_extract(observed_time, "^\\d{2}"))) %>% 
+  mutate(time_of_day = 
+           case_when(arrival_hour <= 6  ~ "Early morning (00:00 to 6:59)",
+                     arrival_hour <= 9  ~ "Morning rush (07:00 to 9:59)",
+                     arrival_hour <= 14 ~ "Midday (10:00 to 14:59)",
+                     arrival_hour <= 18 ~ "Afternoon rush (15:00 to 18:59)",
+                     arrival_hour <= 23 ~ "Evening (19:00 to 23:59)")) %>% 
+  mutate(time_of_day = 
+           fct_relevel(time_of_day,
+                       "Early morning (00:00 to 6:59)",
+                       "Morning rush (07:00 to 9:59)",
+                       "Midday (10:00 to 14:59)",
+                       "Afternoon rush (15:00 to 18:59)",
+                       "Evening (19:00 to 23:59)"))
+
+# Directions often have multiple headsigns; pick the most common per direction
+directions <- arrivals_ready %>% 
+  count(route_id, direction_id, trip_headsign) %>% 
+  arrange(route_id, direction_id, desc(n)) %>% 
+  slice_max(order_by = n, by = c(route_id, direction_id), with_ties = FALSE)
+  
+# Aggregate number of arrivals
+arrivals_aggregated <- arrivals_ready %>% 
+  mutate(stop_id = as.character(stop_id)) %>% 
+  group_by(period, route_id, direction_id, week_period, stop_id, stop_name, time_of_day) %>% 
+  summarize(arrivals = n()) %>% 
+  ungroup() %>% 
+  # Join prototypical direction headsigns
+  left_join(directions %>% select(-n)) %>% 
+  # Normalize counts to be arrivals per day
+  mutate(arrivals = if_else(week_period == "Weekday", arrivals / 5, arrivals / 2)) %>% 
+  # Pivot periods to top
+  pivot_wider(names_from = period,
+              values_from = arrivals,
+              names_prefix = "arrivals_",
+              values_fill = 0) %>% 
+  # Percentage loss
+  mutate(percent_change = (arrivals_during_cuts / arrivals_before_cuts) - 1) %>% 
+  mutate(percent_change = scales::label_percent()(percent_change))
+
+# Post-cut network data --------------------------------------------------------------------------------
+
+network_raw <- read_gtfs(str_c(data_dir, "raw", "septa_cut", "google_bus.zip", sep = "/"))
 
 # Read shapes and stops as sf objects for mapping
-normal_sf <- gtfs_as_sf(normal_raw)
+network_sf <- gtfs_as_sf(network_raw)
 
 # Route linestrings
-normal_geometry <- normal_sf[["shapes"]]
+network_geometry <- network_sf[["shapes"]]
 
-normal_routes <- normal_sf[["routes"]]
+network_trips <- network_raw[["trips"]]
 
-normal_trips <- normal_sf[["trips"]]
+network_stop_times <- network_raw[["stop_times"]]
 
-# Post-cut data --------------------------------------------------------------------------------
+network_stops <- network_raw[["stops"]] %>% 
+  select(stop_id, stop_lat, stop_lon) %>% 
+  st_as_sf(coords = c("stop_lon", "stop_lat"), crs = "EPSG:4326")
 
-cut_raw <- read_gtfs(str_c(data_dir, "raw", "septa_cut", "google_bus.zip", sep = "/"))
-
-# Read shapes and stops as sf objects for mapping
-cut_sf <- gtfs_as_sf(cut_raw)
-
-# Route linestrings
-cut_geometry <- cut_sf[["shapes"]]
-
-cut_routes <- cut_raw[["routes"]]
-
-cut_trips <- cut_raw[["trips"]]
-
-# Collating information -----------------------------------------------------------------------
-
-# Identify routes which do not exist post-cut
-routes_eliminated <- cut_routes %>% 
-  filter(str_detect(route_long_name, "ELIMINATED")) %>% 
-  pull(route_id)
-
-# Identify routes with reduced stops
-# From: https://billypenn.com/2025/08/24/septa-cuts-routes-fares-august-2025/
-routes_shortened <- 
-  c(2, 3, 5, 7, 9, 17, 27, 43, 61, 84, 115, 124, 125, 433, 441, 495)
+# Collate data --------------------------------------------------------------------------------
 
 # Associate linestrings with route numbers
-normal_mapping_data_all <- normal_geometry %>% 
-  left_join(normal_trips %>% distinct(shape_id, route_id)) %>% 
-  mutate(cut_status = 
-           case_when(route_id %in% routes_eliminated ~ "Eliminated",
-                     route_id %in% routes_shortened ~ "Shortened",
-                     .default = "Remaining"))
+network_routes_mapping <- network_geometry %>% 
+  left_join(network_trips %>% distinct(shape_id, route_id)) %>% 
+  # Only routes in arrivals data
+  filter(route_id %in% arrivals_aggregated$route_id)
 
-# Associate linestrings with route numbers
-cut_mapping_data_all <- cut_geometry %>% 
-  left_join(cut_trips %>% distinct(shape_id, route_id)) %>% 
-  mutate(cut_status = 
-           case_when(route_id %in% routes_eliminated ~ "Eliminated",
-                     route_id %in% routes_shortened ~ "Shortened",
-                     .default = "Remaining"))
-
-# Get district-level summary ------------------------------------------------------------------
-
-district_cuts <- normal_mapping_data_all %>% 
-  st_join(districts) %>% 
-  st_drop_geometry() %>% 
-  distinct(route_id, cut_status, leg_district_no) %>% 
-  group_by(leg_district_no, cut_status) %>% 
-  summarize(routes = n()) %>% 
-  mutate(percent = routes / sum(routes)) %>% 
-  pivot_wider(names_from = cut_status, values_from = c(routes, percent), values_fill = 0) %>% 
-  clean_names() %>% 
-  mutate(across(contains("percent"), ~ scales::label_percent()(.))) %>%
-  right_join(districts, by = "leg_district_no") %>% 
-  st_as_sf()
-
+# Arrivals data with stop coords
+arrivals_export <- network_stops %>%
+  inner_join(arrivals_aggregated %>% select(-direction_id))
+  
 # Export --------------------------------------------------------------------------------------
 
-st_write(normal_mapping_data_all, 
-             str_c(data_dir, "/overall_network.json"), 
-             driver = "GeoJSON")
+st_write(network_routes_mapping, 
+         str_c(data_dir, "/septa_routes.json"), 
+         driver = "GeoJSON")
 
-st_write(district_cuts, 
-         str_c(data_dir, "/districts.json"), 
+st_write(arrivals_export, 
+         str_c(data_dir, "/arrivals_and_stops.json"), 
          driver = "GeoJSON")
 
 
